2542436ee851a815d5ff55cf5a7b2bc125a9f43c
nanyugua <108612056+nanyugua@users.noreply.github.com>
2022-07-03 23:04:06 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -2,7 +2,7 @@ package com.atguigu.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git!");
+        System.out.println("hello git11111111111111111111!");
         System.out.println("hello git2!");
         System.out.println("hello git3!");
         System.out.println("hello git44444!");
